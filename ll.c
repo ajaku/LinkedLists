d51fbc9b1f3f1be0843f5f9bb4df7d8e4ae832e1
ajaku <andyjaku13@gmail.com>
2023-02-27 18:10:51 -0500

    Final code for `ll.c`

--- ll.c
+++ ll.c
@@ -25,17 +25,12 @@ int list_get(list_t *list, int *val, size_t pos);
 /* my own fcn */ void list_loop(list_t *list);
 
 list_t *list_alloc(void) {
-  list_t *list;
-  list = (list_t *)malloc(sizeof(list_t));
-  list->head = NULL;
-  list->tail = NULL;
-  list->size = 0;
-  return list;
+  return calloc(1,sizeof(list_t));
 }
 
 void list_free(list_t *list) {
   node_t *temp;
-  while(list->head != NULL) {
+  while(list->head) {
       temp = list->head;
       list->head = list->head->next;
       free(temp);
@@ -44,7 +39,8 @@ void list_free(list_t *list) {
 }
 
 int list_prepend(list_t *list, int val) {
-  node_t *temp = (node_t *)malloc(sizeof(node_t));
+  node_t *temp = malloc(sizeof(*temp));
+  if(!temp) return -1;
   temp->data = val;
   temp->next = list->head;
   list->head = temp;
@@ -56,87 +52,93 @@ int list_prepend(list_t *list, int val) {
 }
 
 int list_append(list_t *list, int val) {
-  node_t *temp = (node_t *)malloc(sizeof(node_t));
+  node_t *temp = malloc(sizeof(*temp));
+  if(!temp) return -1;
   temp->data = val;
   temp->next = NULL;
-  if(list->tail) {
-    list->tail->next = temp;
-    list->tail = temp;
-    list->size++;
-    return 0;
-  }
-  list->head = temp;
-  list->tail = temp;
+
+  list->tail = (list->tail) ? (list->tail->next = temp) : (list->head = temp);
+
   list->size++;
   return 0;
 }
 
-/*int list_insert(list_t *list, int val, size_t pos) {
-  if (!list->head) {
-    list_prepend(list, val);
-  }
-  if (list->size < pos) {
-    printf("Linked list is not large enough\n");
-  } else {
-
-    if (pos == 0) {
-      list_prepend(list, val);
-    } else {
-      node_t *temp = malloc(sizeof(node_t));
-      node_t *it;
-      temp->data = val;
-      int counter = 0;
-      it = list->head;
-      while (counter < pos - 1) {
-        it = it->next;
-        counter++;
-      }
-      temp->next = it->next;
-      it->next = temp;
-      list->size++;
-    }
-  }
-  return 0;
-}*/
-
 int list_insert(list_t *list, int val, size_t pos) {
-  if(!list->head || pos == 0) { list_prepend(list, val); return 0; }
-  if(list->size < pos) { return 1; }
+  if (!pos) return list_prepend(list, val);
+  if (pos == list->size) return list_append(list, val);
 
-  int i = 0;
+  if(list->size < pos) return -1;
+
   node_t *temp = malloc(sizeof(node_t));
-  node_t *it;
-  temp->data = val;
-  it = list->head;
-  while(i < pos-1) {
-    it = it->next;
-    i++;
-  }
+  if (!temp) return -1;
+
+  node_t *it = list->head;
+  while (--pos) it = it->next;
+
   temp->next = it->next;
+  temp->data = val;
   it->next = temp;
+
   list->size++;
   return 0;
 }
 
 int list_rm(list_t *list, int *val, size_t pos) {
-  if(!list->head) { return 1; }
-  if(pos == list->size) { 
-    int stored = list->tail->data;
-    free(list->tail); 
-    return stored;
+  if (pos > list->size) return -1;
+
+  node_t *it = list->head;
+
+  if (!pos) {
+    if (val) *val = it->data;
+    list->head = it->next;
+    if (list->head) list->tail = NULL;
+  } else {
+    while (--pos) it = it->next;
+
+    if (val) *val = it->next->data;
+
+    node_t *temp = it;
+    it = it->next;
+
+    temp->next = temp->next->next;
   }
- 
-  int i = 0;
-  node_t *temp;
-  temp = list->head;
-  while(i < pos-1){
-    temp = temp->next;
-    i++;
+
+  free(it);
+
+  list->size--;
+  return 0;
+}
+
+int list_set(list_t *list, int val, size_t pos) {
+  if (pos > list->size) return -1;
+
+  if (pos == list->size - 1) {
+    list->tail->data = val;
+    return 0;
   }
-  int stored = temp->next->data;
-  temp = temp->next->next;
-  free(temp->next);
-  return stored;
+
+  node_t *it = list->head;
+  while (pos--) it = it->next;
+
+  it->data = val;
+
+  return 0;
+}
+
+int list_get(list_t *list, int *val, size_t pos) {
+  if(pos > list->size) return -1;
+
+  if(pos == list->size - 1) {
+    if (val) *val = list->tail->data;
+    return 0;
+  }
+  
+  node_t *it = list->head;
+  while (pos--) it = it->next;
+
+  if (val) *val = it->data;
+
+  return 0;
 }
 
 void list_loop(list_t *list) {
@@ -151,19 +153,31 @@ void list_loop(list_t *list) {
 
 int main(void) {
   list_t *list = list_alloc();  
+  puts("append 4");
   list_append(list, 4);
   list_loop(list);
+  puts("prepend 10");
   list_prepend(list, 10);
   list_loop(list);
+  puts("prepend 20");
   list_prepend(list, 20);
   list_loop(list);
+  puts("append 5");
   list_append(list, 5);
   list_loop(list);
+  puts("insert 100 at 2");
   list_insert(list, 100, 2);
   list_loop(list);
-  // 20, 10, 100, 4, 5
-  int removed = list_rm(list, &removed, 0);
-  // 20, 10, 4, 5
+  puts("removed at 3");
+  int removed;
+  list_rm(list, &removed, 3);
+  printf("removed: %d\n", removed);
+  puts("set 21 at 3");
+  list_set(list, 21, 3);
   list_loop(list);
+  puts("getting at 3");
+  int get;
+  list_get(list, &get, 3);
+  printf("Got\t%d\n", get);
   list_free(list);
 }
